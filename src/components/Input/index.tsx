ff5db1c102af8373f01280f9de79f18ea31d9b22
Felipe Pimentel <felipekdra@gmail.com>
2022-09-12 19:18:50 -0300

    refactor: adding types to Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -9,7 +9,13 @@ import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
+type InputProps = {
+  name: string;
+  icon: React.ElementType;
+  placeholder?: string;
+}
+
+const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
   const inputRef = useRef(null);
 
   const [isFocused, setIsFocused] = useState(false);
@@ -24,7 +30,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
   const handleInputBlur = useCallback(() => {
     setIsFocused(false);
 
-    setIsFilled(!!inputRef.current?.value);
+    setIsFilled(!!inputRef.current?.['value']);
   }, []);
 
   useEffect(() => {
